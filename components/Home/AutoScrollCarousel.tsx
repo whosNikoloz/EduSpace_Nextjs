b03b9cfe553d47837b089c227695e80f76559617
whosNikoloz <nika.kobaidze04@gmail.com>
2023-09-29 20:47:45 +0400

    carusel skeleton

--- components/Home/AutoScrollCarousel.tsx
+++ components/Home/AutoScrollCarousel.tsx
@@ -8,6 +8,7 @@ import { isMobile } from "react-device-detect";
 import ilustration from "@/public/ilustration2.png";
 import Courses from "@/app/api/Learn/Course";
 import Image from "next/image";
+import { Skeleton } from "@nextui-org/react";
 
 interface Course {
   courseId: number;
@@ -27,15 +28,12 @@ const AutoScrollCarousel = () => {
       try {
         const response = await courseAPI.GetCourses();
         setCourses(response); // Assuming the API response is an array of Course objects
-        console.log(response);
-        console.log(response);
       } catch (error) {
         console.error("Error fetching courses:", error);
       }
     };
 
     fetchCourses();
-    console.log(courses);
   }, []);
 
   useEffect(() => {
@@ -88,26 +86,62 @@ const AutoScrollCarousel = () => {
               </li>
             ))
           ) : (
-            <li className="splide__slide">
-              <div className="slide-content">
-                <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                  <a href="#">
-                    <Image
-                      className="rounded-t-lg"
-                      src={ilustration}
-                      alt="Courses"
-                    />
-                  </a>
-                  <div className="p-3">
+            <>
+              <li className="splide__slide">
+                <div className="slide-content">
+                  <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <a href="#">
-                      <h5 className="mb-2 text-base font-bold tracking-tight text-gray-900 dark:text-white">
-                        იტვირთება
-                      </h5>
+                      <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+                        <svg
+                          className="w-10 h-10 text-gray-200 dark:text-gray-600"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 16 20"
+                        >
+                          <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                          <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                        </svg>
+                      </div>
                     </a>
+                    <div className="p-3">
+                      <a href="#">
+                        <Skeleton className="w-4/5 rounded-lg">
+                          <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
+                        </Skeleton>
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </li>
+              </li>
+              <li className="splide__slide">
+                <div className="slide-content">
+                  <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                      <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+                        <svg
+                          className="w-10 h-10 text-gray-200 dark:text-gray-600"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 16 20"
+                        >
+                          <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                          <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                        </svg>
+                      </div>
+                    </a>
+                    <div className="p-3">
+                      <a href="#">
+                        <Skeleton className="w-4/5 rounded-lg">
+                          <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
+                        </Skeleton>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </>
           )}
         </ul>
       </div>
