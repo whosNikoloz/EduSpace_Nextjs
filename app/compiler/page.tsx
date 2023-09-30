416339b91ec79e79f7688787561b1cf99b5612fb
whosNikoloz <nika.kobaidze04@gmail.com>
2023-09-30 23:57:42 +0400

    cleaned compiler

--- app/compiler/page.tsx
+++ app/compiler/page.tsx
@@ -1,13 +1,8 @@
 "use client";
 
 import React, { Key, useState, useEffect, useRef } from "react";
-import Landing from "@/components/compiler/Landing";
 import LayoutNavbar from "../layouts/LayoutNavbar";
 
 export default function CompilerPage() {
-  return (
-    <LayoutNavbar>
-      <Landing />
-    </LayoutNavbar>
-  );
+  return <LayoutNavbar>s</LayoutNavbar>;
 }
