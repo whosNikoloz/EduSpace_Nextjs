b2272190347cfaaa6a89e2b131a6d08e94f1f3e5
whosNikoloz <nika.kobaidze04@gmail.com>
2023-11-02 14:57:59 +0400

    updated Modal postcard and commnetuser

--- components/social/PostCard.jsx
+++ components/social/PostCard.jsx
@@ -14,11 +14,18 @@ import {
   DropdownTrigger,
   DropdownMenu,
   DropdownItem,
-  Button,
 } from "@nextui-org/react";
 import { useUser } from "@/app/context/UserdbContext";
 import Comment from "@/components/social/commentUser";
-import WarningAlert from "./WarningAlert";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@nextui-org/react";
 
 function formatTimeAgo(timestamp) {
   const currentDate = new Date();
@@ -43,6 +50,8 @@ function PostCard({ postData, onDelete }) {
   const { user } = useUser();
   const [IsAddingComment, setIsAddingComment] = useState(false);
 
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   const handleNewComment = () => {
     // Set a flag to indicate that a new comment is created
     setIsAddingComment(true);
@@ -52,22 +61,13 @@ function PostCard({ postData, onDelete }) {
   };
   const [isFullTextVisible, setFullTextVisible] = useState(false);
 
-  const [isOpen, setIsOpen] = useState(false);
-  const [isDeleting, setIsDeleting] = useState(false);
-
-  const [showWarningAlert, setShowWarningAlert] = useState(false);
-
+  const [isOpenPost, setIsOpenPost] = useState(false);
   const toggleFullText = () => {
     setFullTextVisible(!isFullTextVisible);
   };
 
-  const confirmDelete = () => {
-    setShowWarningAlert(true);
-  };
-
   const handleDelete = async () => {
     const post = Posts();
-    setIsDeleting(true);
     try {
       await post.DeletePost(
         postData.postId,
@@ -75,13 +75,10 @@ function PostCard({ postData, onDelete }) {
         postData.picture,
         postData.comments
       );
-      setIsDeleting(false);
-      console.log(postData.comments);
-      setIsOpen(false); // Close the dialog after deletion
+      setIsOpenPost(false); // Close the dialog after deletion
       onDelete(postData.postId); // Notify the parent component about the deletion
       setShowAlert(true); // Show an alert to confirm that the post has been deleted
     } catch (error) {
-      setIsDeleting(false);
       console.error("Error deleting post:", error);
     }
   };
@@ -121,7 +118,8 @@ function PostCard({ postData, onDelete }) {
                     key="delete"
                     className="text-danger"
                     color="danger"
-                    onClick={confirmDelete}
+                    
+                    onPress={onOpen}
                   >
                     პოსტის წაშლა
                   </DropdownItem>
@@ -156,7 +154,7 @@ function PostCard({ postData, onDelete }) {
                 />
               )}
               {postData.picture && (
-                <button onClick={() => setIsOpen(true)}>
+                <button onClick={() => setIsOpenPost(true)}>
                   <img
                     className="w-auto max-h-screen rounded"
                     src={postData.picture}
@@ -171,7 +169,7 @@ function PostCard({ postData, onDelete }) {
             <Link>
               <button
                 className="ml-1 text-gray-500 dark:text-gray-400 font-light"
-                onClick={() => setIsOpen(true)}
+                onClick={() => setIsOpenPost(true)}
               >
                 {commentCount === 0 ? "კომენტარი" : `${commentCount} კომენტარი`}
               </button>
@@ -179,21 +177,68 @@ function PostCard({ postData, onDelete }) {
           </div>
         </div>
       </div>
-      {showWarningAlert && (
-        <WarningAlert
-          title="პოსტის წაშლა"
-          message="დარწმუნებული ხართ, რომ გსურთ ამ პოსტის წაშლა?"
-          onCancel={() => setShowWarningAlert(false)}
-          onAgree={() => {
-            setShowWarningAlert(false);
-            handleDelete();
-          }}
-        />
-      )}
-
+      <Modal
+        backdrop="blur"
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        radius="2xl"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                პოსტის წაშლა
+              </ModalHeader>
+              <ModalBody>
+                <div className="flex flex-row gap-2">
+                  <svg
+                    height="50px"
+                    width="50px"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 473.931 473.931"
+                    xmlSpace="preserve"
+                    fill="#000000"
+                  >
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <circle
+                        style={{ fill: "#E84849" }}
+                        cx="236.966"
+                        cy="236.966"
+                        r="236.966"
+                      ></circle>
+                      <path
+                        style={{ fill: "#EDC92C" }}
+                        d="M409.266,333.9L246.676,71.853c-1.893-3.057-5.231-4.913-8.823-4.913 c-3.596,0-6.933,1.86-8.827,4.913L65.997,334.618c-1.987,3.203-2.088,7.233-0.251,10.526c1.83,3.293,5.31,5.336,9.074,5.336h326.072 h0.045c5.736,0,10.383-4.651,10.383-10.383C411.313,337.772,410.553,335.632,409.266,333.9z"
+                      ></path>
+                      <path d="M225.819,242.111l-3.371-50.439c-0.632-9.83-0.943-16.887-0.943-21.167c0-5.826,1.527-10.372,4.576-13.635 c3.053-3.274,7.079-4.902,12.06-4.902c6.039,0,10.08,2.088,12.112,6.264c2.032,4.18,3.053,10.204,3.053,18.058 c0,4.636-0.247,9.347-0.733,14.11l-4.531,51.917c-0.49,6.181-1.542,10.918-3.162,14.222c-1.616,3.296-4.288,4.943-8.004,4.943 c-3.794,0-6.425-1.59-7.895-4.789C227.503,253.504,226.448,248.64,225.819,242.111z M237.508,311.401 c-4.284,0-8.026-1.381-11.214-4.153c-3.195-2.769-4.789-6.649-4.789-11.633c0-4.355,1.527-8.06,4.576-11.117 c3.053-3.053,6.795-4.58,11.218-4.58c4.426,0-3.053,4.913-1.579,8.771-4.745,11.581C245.403,309.997,241.721,311.401,237.508,311.401z"></path>
+                    </g>
+                  </svg>
+                  <p>დარწმუნებულიხართ რომ გსურთ ამ პოსტის წაშლა ?</p>
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="foreground" variant="light" onPress={onClose}>
+                  დახურვა
+                </Button>
+                <Button onClick={handleDelete} color="primary" variant="shadow">
+                  წაშლა
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
       <Dialog
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
+        open={isOpenPost}
+        onClose={() => setIsOpenPost(false)}
         className="fixed z-10 inset-0 "
       >
         <div className="flex items-center justify-center min-h-[80vh] mt-10">
@@ -210,7 +255,7 @@ function PostCard({ postData, onDelete }) {
               <div className="absolute right-0 p-2">
                 <button
                   className="p-2 rounded-full text-black dark:text-white"
-                  onClick={() => setIsOpen(false)}
+                  onClick={() => setIsOpenPost(false)}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
--- components/social/commentUser.tsx
+++ components/social/commentUser.tsx
@@ -7,11 +7,19 @@ import {
   DropdownTrigger,
   DropdownMenu,
   DropdownItem,
-  Button,
 } from "@nextui-org/react";
 import { DotsIcon } from "@/components/social/DotsIcon";
 import { useUser } from "@/app/context/UserdbContext";
-import WarningAlert from "@/components/social/WarningAlert";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@nextui-org/react";
+import toast, { Toaster } from "react-hot-toast";
 
 interface CommentProps {
   commentId: string;
@@ -57,24 +65,17 @@ const Comment: React.FC<CommentProps> = ({
 }) => {
   const formattedTimeAgo = formatTimeAgo(createdAt);
   const { user } = useUser();
-  const [isDeleting, setIsDeleting] = useState(false);
 
-  const [showWarningAlert, setShowWarningAlert] = useState(false);
-
-  const confirmDelete = () => {
-    setShowWarningAlert(true);
-  };
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const handleDelete = () => {
     const comment = CommentApi();
-    setIsDeleting(true);
     comment
       .DeleteComment(commentId, videoUrl, pictureUrl)
       .then(() => {
-        setIsDeleting(false);
+        toast.success("წარმატებით წაიშალა");
       })
       .catch((error) => {
-        setIsDeleting(false);
         console.error("Error deleting post:", error);
       });
   };
@@ -133,25 +134,76 @@ const Comment: React.FC<CommentProps> = ({
                 key="delete"
                 className="text-danger"
                 color="danger"
-                onClick={confirmDelete}
+                onPress={onOpen}
               >
                 კომენტარის წაშლა
               </DropdownItem>
             </DropdownMenu>
           </Dropdown>
         )}
-        {showWarningAlert && (
-          <WarningAlert
-            title="კომენტარის წაშლა"
-            message="
-            დარწმუნებული ხართ რომ გსურთ ამ კომენტარის წაშლა?"
-            onCancel={() => setShowWarningAlert(false)}
-            onAgree={() => {
-              setShowWarningAlert(false);
-              handleDelete();
-            }}
-          />
-        )}
+        <Modal
+          backdrop="blur"
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+          radius="lg"
+        >
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  პოსტის წაშლა
+                </ModalHeader>
+                <ModalBody>
+                  <div className="flex flex-row gap-2">
+                    <svg
+                      height="50px"
+                      width="50px"
+                      version="1.1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlnsXlink="http://www.w3.org/1999/xlink"
+                      viewBox="0 0 473.931 473.931"
+                      xmlSpace="preserve"
+                      fill="#000000"
+                    >
+                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></g>
+                      <g id="SVGRepo_iconCarrier">
+                        <circle
+                          style={{ fill: "#E84849" }}
+                          cx="236.966"
+                          cy="236.966"
+                          r="236.966"
+                        ></circle>
+                        <path
+                          style={{ fill: "#EDC92C" }}
+                          d="M409.266,333.9L246.676,71.853c-1.893-3.057-5.231-4.913-8.823-4.913 c-3.596,0-6.933,1.86-8.827,4.913L65.997,334.618c-1.987,3.203-2.088,7.233-0.251,10.526c1.83,3.293,5.31,5.336,9.074,5.336h326.072 h0.045c5.736,0,10.383-4.651,10.383-10.383C411.313,337.772,410.553,335.632,409.266,333.9z"
+                        ></path>
+                        <path d="M225.819,242.111l-3.371-50.439c-0.632-9.83-0.943-16.887-0.943-21.167c0-5.826,1.527-10.372,4.576-13.635 c3.053-3.274,7.079-4.902,12.06-4.902c6.039,0,10.08,2.088,12.112,6.264c2.032,4.18,3.053,10.204,3.053,18.058 c0,4.636-0.247,9.347-0.733,14.11l-4.531,51.917c-0.49,6.181-1.542,10.918-3.162,14.222c-1.616,3.296-4.288,4.943-8.004,4.943 c-3.794,0-6.425-1.59-7.895-4.789C227.503,253.504,226.448,248.64,225.819,242.111z M237.508,311.401 c-4.284,0-8.026-1.381-11.214-4.153c-3.195-2.769-4.789-6.649-4.789-11.633c0-4.355,1.527-8.06,4.576-11.117 c3.053-3.053,6.795-4.58,11.218-4.58c4.426,0-3.053,4.913-1.579,8.771-4.745,11.581C245.403,309.997,241.721,311.401,237.508,311.401z"></path>
+                      </g>
+                    </svg>
+                    <p>დარწმუნებულიხართ რომ გსურთ ამ კომენტარის წაშლა ?</p>
+                  </div>
+                </ModalBody>
+                <ModalFooter>
+                  <Button variant="light" onPress={onClose}>
+                    დახურვა
+                  </Button>
+                  <Button
+                    onClick={handleDelete}
+                    color="primary"
+                    variant="shadow"
+                  >
+                    წაშლა
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </div>
     </div>
   );
