a3aa93b5e6a93cc098a11d0a9122231a6185e50f
whosNikoloz <nika.kobaidze04@gmail.com>
2023-11-24 15:34:49 +0400

    adedd AIChat

--- components/AIChat/AIChat.jsx
+++ components/AIChat/AIChat.jsx
@@ -1,6 +1,9 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import { useChat } from "ai/react";
+import { useUser } from "@/app/dbcontext/UserdbContext";
+
 import {
   Modal,
   ModalContent,
@@ -13,10 +16,13 @@ import {
 
 export const ChatAI = () => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { user, logout } = useUser();
 
   return (
     <>
       <Modal
+        scrollBehavior="inside"
         backdrop="opaque"
         isOpen={isOpen}
         onOpenChange={onOpenChange}
@@ -31,7 +37,7 @@ export const ChatAI = () => {
               },
             },
             exit: {
-              x: -20,
+              x: 20,
               opacity: 0,
               transition: {
                 duration: 0.2,
@@ -47,10 +53,10 @@ export const ChatAI = () => {
               <ModalHeader className="flex flex-col gap-1">
                 <div className="flex flex-col space-y-1.5 pb-6">
                   <h2 className="font-semibold text-lg tracking-tight">
-                    Chatbot
+                    EduSpace Chat
                   </h2>
                   <p className="text-sm text-[#6b7280] leading-3">
-                    Powered by Mendable and Vercel
+                    Powered by Nika
                   </p>
                 </div>
               </ModalHeader>
@@ -59,98 +65,52 @@ export const ChatAI = () => {
                   className="pr-4 h-[474px]"
                   style={{ minWidth: "100%", display: "table" }}
                 >
-                  <div className="flex gap-3 my-4 text-gray-600 text-sm flex-1">
-                    <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
-                      <div className="rounded-full bg-gray-100 border p-1">
-                        <svg
-                          stroke="none"
-                          fill="black"
-                          stroke-width="1.5"
-                          viewBox="0 0 24 24"
-                          aria-hidden="true"
-                          height="20"
-                          width="20"
-                          xmlns="http://www.w3.org/2000/svg"
+                  {messages.length > 0
+                    ? messages.map((m) => (
+                        <div
+                          key={m.id}
+                          className="flex gap-3 my-4 text-gray-600 dark:text-white text-sm flex-1"
                         >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
-                          ></path>
-                        </svg>
-                      </div>
-                    </span>
-                    <p className="leading-relaxed">
-                      <span className="block font-bold text-gray-700">AI </span>
-                      Hi, how can I help you today?
-                    </p>
-                  </div>
-
-                  <div className="flex gap-3 my-4 text-gray-600 text-sm flex-1">
-                    <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
-                      <div className="rounded-full bg-gray-100 border p-1">
-                        <svg
-                          stroke="none"
-                          fill="black"
-                          stroke-width="0"
-                          viewBox="0 0 16 16"
-                          height="20"
-                          width="20"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"></path>
-                        </svg>
-                      </div>
-                    </span>
-                    <p className="leading-relaxed">
-                      <span className="block font-bold text-gray-700">
-                        You{" "}
-                      </span>
-                      fewafef
-                    </p>
-                  </div>
-                  <div className="flex gap-3 my-4 text-gray-600 text-sm flex-1">
-                    <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
-                      <div className="rounded-full bg-gray-100 border p-1">
-                        <svg
-                          stroke="none"
-                          fill="black"
-                          stroke-width="1.5"
-                          viewBox="0 0 24 24"
-                          aria-hidden="true"
-                          height="20"
-                          width="20"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
-                          ></path>
-                        </svg>
-                      </div>
-                    </span>
-                    <p className="leading-relaxed">
-                      <span className="block font-bold text-gray-700">AI </span>
-                      Sorry, I couldn&apos;t find any information in the
-                      documentation about that. Expect the answer to be less
-                      accurate. I could not find the answer to this in the
-                      verified sources.
-                    </p>
-                  </div>
+                          <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
+                            <div className="rounded-full bg-gray-100">
+                              {m.role === "user" ? (
+                                <>
+                                  <img src={user?.picture} alt="user" />
+                                </>
+                              ) : (
+                                <EduSpace />
+                              )}
+                            </div>
+                          </span>
+                          <p className="leading-relaxed dark:text-white">
+                            <span className="block font-bold text-gray-700 dark:text-blue-600">
+                              {m.role === "user" ? user?.userName : "EduSpace"}
+                            </span>
+                            {m.content}
+                          </p>
+                        </div>
+                      ))
+                    : null}
                 </div>
               </ModalBody>
               <ModalFooter>
                 <div className="flex items-center pt-0">
-                  <form className="flex items-center justify-center w-full space-x-2">
+                  <form
+                    className="flex items-center justify-center w-full space-x-2"
+                    onSubmit={handleSubmit}
+                  >
                     <input
-                      className="flex h-10 w-full rounded-md border border-[#e5e7eb] px-3 py-2 text-sm placeholder-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#9ca3af] disabled:cursor-not-allowed disabled:opacity-50 text-[#030712] focus-visible:ring-offset-2"
+                      className="flex h-10 w-full rounded-lg border border-[#e5e7eb] px-3 py-2 dark:text-white text-sm placeholder-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#9ca3af] disabled:cursor-not-allowed disabled:opacity-50 text-[#030712] focus-visible:ring-offset-2"
                       placeholder="Type your message"
-                      value=""
+                      onChange={handleInputChange}
+                      value={input}
                     />
-                    <button className="inline-flex items-center justify-center rounded-md text-sm font-medium text-[#f9fafb] disabled:pointer-events-none disabled:opacity-50 bg-black hover:bg-[#111827E6] h-10 px-4 py-2">
+                    <Button
+                      type="submit"
+                      className="text-sm font-medium bg-black text-white  hover:bg-blue-600 h-10 px-4 py-2"
+                    >
                       Send
-                    </button>
+                    </Button>
                   </form>
                 </div>
               </ModalFooter>
--- components/FAB.tsx
+++ components/FAB.tsx
@@ -6,6 +6,8 @@ import { Button } from "@nextui-org/button";
 import { UpVectorIcon } from "./UpVectorIcon";
 import { EduSpace } from "./EduSpaceLogo";
 import { ChatIcon } from "./ChatIcon";
+import { useChat } from "ai/react";
+import { useUser } from "@/app/dbcontext/UserdbContext";
 import {
   Modal,
   ModalContent,
@@ -17,6 +19,8 @@ import {
 
 const FAB: React.FC = () => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { user, logout } = useUser();
 
   // Function to scroll to the top when the button is clicked
   const scrollToTop = () => {
@@ -73,10 +77,10 @@ const FAB: React.FC = () => {
                 <ModalHeader className="flex flex-col gap-1">
                   <div className="flex flex-col space-y-1.5 pb-6">
                     <h2 className="font-semibold text-lg tracking-tight">
-                      Chatbot
+                      EduSpace Chat
                     </h2>
                     <p className="text-sm text-[#6b7280] leading-3">
-                      Powered by Mendable and Vercel
+                      Powered by Nika
                     </p>
                   </div>
                 </ModalHeader>
@@ -85,100 +89,52 @@ const FAB: React.FC = () => {
                     className="pr-4 h-[474px]"
                     style={{ minWidth: "100%", display: "table" }}
                   >
-                    <div className="flex gap-3 my-4 text-gray-600 dark:text-white text-sm flex-1">
-                      <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
-                        <div className="rounded-full bg-gray-100 border p-1">
-                          <svg
-                            stroke="none"
-                            fill="black"
-                            stroke-width="1.5"
-                            viewBox="0 0 24 24"
-                            aria-hidden="true"
-                            height="20"
-                            width="20"
-                            xmlns="http://www.w3.org/2000/svg"
+                    {messages.length > 0
+                      ? messages.map((m) => (
+                          <div
+                            key={m.id}
+                            className="flex gap-3 my-4 text-gray-600 dark:text-white text-sm flex-1"
                           >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
-                            ></path>
-                          </svg>
-                        </div>
-                      </span>
-                      <p className="leading-relaxed dark:text-white">
-                        <span className="block font-bold text-gray-700 dark:text-blue-600">
-                          AI{" "}
-                        </span>
-                        Hi, how can I help you today?
-                      </p>
-                    </div>
-
-                    <div className="flex gap-3 my-4 text-gray-600 text-sm flex-1">
-                      <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
-                        <div className="rounded-full bg-gray-100 border p-1">
-                          <svg
-                            stroke="none"
-                            fill="black"
-                            stroke-width="0"
-                            viewBox="0 0 16 16"
-                            height="20"
-                            width="20"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"></path>
-                          </svg>
-                        </div>
-                      </span>
-                      <p className="leading-relaxed dark:text-white">
-                        <span className="block font-bold text-gray-700 dark:text-blue-600">
-                          You{" "}
-                        </span>
-                        fewafef
-                      </p>
-                    </div>
-                    <div className="flex gap-3 my-4 text-gray-600 text-sm flex-1">
-                      <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
-                        <div className="rounded-full bg-gray-100 border p-1">
-                          <svg
-                            stroke="none"
-                            fill="black"
-                            stroke-width="1.5"
-                            viewBox="0 0 24 24"
-                            aria-hidden="true"
-                            height="20"
-                            width="20"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
-                            ></path>
-                          </svg>
-                        </div>
-                      </span>
-                      <p className="leading-relaxed dark:text-white">
-                        <span className="block font-bold text-gray-700 dark:text-blue-600">
-                          AI{" "}
-                        </span>
-                        Sorry, I couldn&apos;t find any information in the
-                        documentation about that. Expect the answer to be less
-                        accurate. I could not find the answer to this in the
-                        verified sources.
-                      </p>
-                    </div>
+                            <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
+                              <div className="rounded-full bg-gray-100">
+                                {m.role === "user" ? (
+                                  <>
+                                    <img src={user?.picture} alt="user" />
+                                  </>
+                                ) : (
+                                  <EduSpace />
+                                )}
+                              </div>
+                            </span>
+                            <p className="leading-relaxed dark:text-white">
+                              <span className="block font-bold text-gray-700 dark:text-blue-600">
+                                {m.role === "user"
+                                  ? user?.userName
+                                  : "EduSpace"}
+                              </span>
+                              {m.content}
+                            </p>
+                          </div>
+                        ))
+                      : null}
                   </div>
                 </ModalBody>
                 <ModalFooter>
                   <div className="flex items-center pt-0">
-                    <form className="flex items-center justify-center w-full space-x-2">
+                    <form
+                      className="flex items-center justify-center w-full space-x-2"
+                      onSubmit={handleSubmit}
+                    >
                       <input
-                        className="flex h-10 w-full rounded-lg border border-[#e5e7eb] px-3 py-2 text-sm placeholder-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#9ca3af] disabled:cursor-not-allowed disabled:opacity-50 text-[#030712] focus-visible:ring-offset-2"
+                        className="flex h-10 w-full rounded-lg border border-[#e5e7eb] px-3 py-2 dark:text-white text-sm placeholder-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#9ca3af] disabled:cursor-not-allowed disabled:opacity-50 text-[#030712] focus-visible:ring-offset-2"
                         placeholder="Type your message"
-                        value=""
+                        onChange={handleInputChange}
+                        value={input}
                       />
-                      <Button className="text-sm font-medium bg-black text-white  hover:bg-blue-600 h-10 px-4 py-2">
+                      <Button
+                        type="submit"
+                        className="text-sm font-medium bg-black text-white  hover:bg-blue-600 h-10 px-4 py-2"
+                      >
                         Send
                       </Button>
                     </form>
