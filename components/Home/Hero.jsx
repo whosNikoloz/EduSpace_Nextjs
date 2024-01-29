5473a3582ea9c02c9eb61fc21894c6e90778bb87
whosNikoloz <nika.kobaidze021@gmail.com>
2024-01-29 22:55:52 +0400

    updated hero the scrolldown fix

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -33,7 +33,7 @@ export const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-end lg:h-[35vh] h-[26vh] text-center">
+      <div className="flex flex-col items-center justify-end lg:h-[35vh] h-[26vh] text-center lg:absolute bottom-0 left-0 right-0">
         <div className="relative h-[30px] w-[19px] rounded-[15px] border-[2px] border-[#fff] border-[solid]">
           <div className="absolute bottom-[20px] left-[4px] top-[4px] w-[8px] animate-[scroller_1500ms_ease-out_infinite] rounded-[8px] bg-[#fff]"></div>
         </div>
