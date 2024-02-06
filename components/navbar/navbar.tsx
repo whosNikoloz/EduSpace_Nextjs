4df374dfb8baf82dad07fb2c9aafccb9e4b0359a
whosNikoloz <nika.kobaidze021@gmail.com>
2024-02-07 00:42:44 +0400

    fixed userLoading

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -72,15 +72,6 @@ export const Navbar = () => {
 
   const [isScrolled, setIsScrolled] = useState(false);
 
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate loading for demonstration purposes
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1); // Replace with your actual data fetching logic
-  }, []);
-
   const handleScroll = () => {
     const scrollY = window.scrollY;
     const isScrolled = scrollY > 50; // Adjust the scroll threshold as needed
@@ -194,87 +185,60 @@ export const Navbar = () => {
               </div>
             </div>
             <div className="pr-2 absolute inset-y-0 right-0 flex items-center  sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              {isLoading ? (
+              {user ? (
                 <>
+                  <Notification userid={user.userId} />
                   <div className="relative ml-3">
-                    <div className="max-w-[300px] w-full flex items-center gap-3">
-                      <div>
-                        <div className="flex items-center mt-4 space-x-3 mb-2.5 animate-pulse">
-                          <svg
-                            className="w-10 h-10 text-gray-200 dark:text-gray-700"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                          >
-                            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
-                          </svg>
-                        </div>
-                      </div>
-                    </div>
+                    <Dropdown placement="bottom-end">
+                      <DropdownTrigger>
+                        <Avatar
+                          isBordered
+                          as="button"
+                          className="transition-transform"
+                          color="primary"
+                          name={user.userName}
+                          size="sm"
+                          src={user.picture}
+                        />
+                      </DropdownTrigger>
+                      <DropdownMenu aria-label="Profile Actions" variant="flat">
+                        <DropdownItem key="profile" className="h-14 gap-2">
+                          <p className="font-semibold">შესულიხარ როგორც</p>
+                          <p className="font-semibold">{user.userName}</p>
+                        </DropdownItem>
+                        <DropdownItem key="settings">
+                          <Link href={`/user/profile`}>პარამეტრები</Link>
+                        </DropdownItem>
+
+                        <DropdownItem key="help_and_feedback">
+                          დახმარება
+                        </DropdownItem>
+                        <DropdownItem
+                          key="logout"
+                          color="danger"
+                          onClick={handleLogout}
+                        >
+                          გამოსვლა
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </Dropdown>
                   </div>
                 </>
               ) : (
+                // Render this content if user is null
                 <>
-                  {user ? (
-                    <>
-                      <Notification userid={user.userId} />
-                      <div className="relative ml-3">
-                        <Dropdown placement="bottom-end">
-                          <DropdownTrigger>
-                            <Avatar
-                              isBordered
-                              as="button"
-                              className="transition-transform"
-                              color="primary"
-                              name={user.userName}
-                              size="sm"
-                              src={user.picture}
-                            />
-                          </DropdownTrigger>
-                          <DropdownMenu
-                            aria-label="Profile Actions"
-                            variant="flat"
-                          >
-                            <DropdownItem key="profile" className="h-14 gap-2">
-                              <p className="font-semibold">შესულიხარ როგორც</p>
-                              <p className="font-semibold">{user.userName}</p>
-                            </DropdownItem>
-                            <DropdownItem key="settings">
-                              <Link href={`/user/profile`}>პარამეტრები</Link>
-                            </DropdownItem>
-
-                            <DropdownItem key="help_and_feedback">
-                              დახმარება
-                            </DropdownItem>
-                            <DropdownItem
-                              key="logout"
-                              color="danger"
-                              onClick={handleLogout}
-                            >
-                              გამოსვლა
-                            </DropdownItem>
-                          </DropdownMenu>
-                        </Dropdown>
-                      </div>
-                    </>
-                  ) : (
-                    // Render this content if user is null
-                    <>
-                      <div className="relative ml-3">
-                        <Link href="/user/auth">
-                          <Button
-                            className="bg-blue-600 text-white"
-                            as={Link}
-                            color="primary"
-                            variant="shadow"
-                          >
-                            დაწყება
-                          </Button>
-                        </Link>
-                      </div>
-                    </>
-                  )}
+                  <div className="relative ml-3">
+                    <Link href="/user/auth">
+                      <Button
+                        className="bg-blue-600 text-white"
+                        as={Link}
+                        color="primary"
+                        variant="shadow"
+                      >
+                        დაწყება
+                      </Button>
+                    </Link>
+                  </div>
                 </>
               )}
             </div>
