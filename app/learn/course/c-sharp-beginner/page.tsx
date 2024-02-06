70125047163bb055774c42f9b8cfd0230c05ba50
whosNikoloz <nika.kobaidze021@gmail.com>
2024-02-07 00:45:19 +0400

    fiexd unAuthorized Login on LearnPage Access

--- app/learn/course/c-sharp-beginner/page.tsx
+++ app/learn/course/c-sharp-beginner/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import dynamic from "next/dynamic";
 
 import CoursesAPI from "@/app/api/Learn/Course";
 import { useUser } from "@/app/dbcontext/UserdbContext";
@@ -9,6 +8,16 @@ import ProgressAPI from "@/app/api/Learn/Progress";
 import MainLayout from "@/app/layouts/Mainlayout";
 import { Hero } from "@/components/Learn/Hero";
 import Subject from "@/components/Learn/subject";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@nextui-org/react";
+import Link from "next/link";
 
 interface Course {
   courseId: number;
@@ -30,32 +39,42 @@ export default function CsharpBeginnerPage() {
   const { user } = useUser();
   const [course, setCourse] = useState<Course | null>(null);
   const [progress, setProgress] = useState<UserProgress | null>(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // Combine the two useEffects into one
   useEffect(() => {
     const fetchCourseAndProgress = async () => {
-      if (!user) return;
-
       try {
         const courseResponse = await CoursesAPI().GetCourse("c-sharp-beginner");
         setCourse(courseResponse);
-
-        const userProgress = await ProgressAPI().GetProgress(
-          { userid: user.userId },
-          { courseid: courseResponse.courseId }
-        );
-        setProgress(userProgress);
+        if (user) {
+          const userProgress = await ProgressAPI().GetProgress(
+            { userid: user.userId },
+            { courseid: courseResponse.courseId }
+          );
+          setProgress(userProgress);
+        } else {
+          setProgress(null);
+          setTimeout(() => {
+            onOpen(); // Open modal if user is not logged in
+          }, 3000);
+        }
       } catch (error) {
         console.error("Error fetching course or user progress:", error);
       }
     };
 
-    fetchCourseAndProgress(); // Fetch course and user progress instantly
+    const fetchData = async () => {
+      await fetchCourseAndProgress();
+      const intervalId = setInterval(fetchCourseAndProgress, 30000);
+      return () => clearInterval(intervalId);
+    };
 
-    const intervalId = setInterval(fetchCourseAndProgress, 30000); // Then fetch course and user progress every 30 seconds
+    fetchData();
 
-    return () => clearInterval(intervalId); // Clean up on unmount or when dependencies change
-  }, [user]); // Fetch course and user progress when user changes
+    return () => {
+      setProgress(null);
+    }; // Clean up course and progress on unmount
+  }, [user]);
 
   return (
     <MainLayout>
@@ -69,6 +88,25 @@ export default function CsharpBeginnerPage() {
           <Subject courseData={course} userProgress={progress} />
         </div>
       )}
+      <Modal backdrop="blur" isOpen={isOpen} onClose={onClose}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">!</ModalHeader>
+              <ModalBody>
+                <p>დასაწყებად აუცილებელია ავტორიზაცია</p>
+              </ModalBody>
+              <ModalFooter>
+                <Link href="/user/auth">
+                  <Button color="primary" onPress={onClose}>
+                    შესვლა
+                  </Button>
+                </Link>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </MainLayout>
   );
 }
