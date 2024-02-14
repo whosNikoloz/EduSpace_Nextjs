d745d9643653ebdde5d2259f430040ecdbaa16d0
whosNikoloz <nika.kobaidze021@gmail.com>
2024-02-14 16:16:08 +0400

    change theme feature + selector

--- components/navbar/Userdropdown.tsx
+++ components/navbar/Userdropdown.tsx
@@ -1,7 +1,13 @@
-import { useState, useEffect, useRef } from "react";
-import { Avatar, Button } from "@nextui-org/react";
+import { useState, useEffect, useRef, ChangeEvent, ReactNode } from "react";
+import { Avatar, Button, Divider, Select, SelectItem } from "@nextui-org/react";
 import Link from "next/link";
-import { Settingicon } from "../icons";
+import {
+  Settingicon,
+  MoonFilledIcon,
+  SunFilledIcon,
+  SystemIcon,
+} from "../icons";
+import { useTheme } from "next-themes";
 
 function UDropdown({
   username,
@@ -16,6 +22,10 @@ function UDropdown({
 }) {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const { resolvedTheme, theme, setTheme } = useTheme();
+
+  const [startCon, setStartCon] = useState<ReactNode>(null);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -34,11 +44,46 @@ function UDropdown({
     };
   }, [isOpen]);
 
+  useEffect(() => {
+    switch (theme) {
+      case "dark":
+        setStartCon(<MoonFilledIcon size={20} height={20} width={20} />);
+        break;
+      case "light":
+        setStartCon(<SunFilledIcon size={20} height={20} width={20} />);
+        break;
+      case "system":
+        setStartCon(<SystemIcon size={20} height={20} width={20} />);
+        break;
+      default:
+        setStartCon(<Settingicon size={20} height={20} width={20} />);
+        break;
+    }
+  }, [theme]);
+
   const handleDropdownClick = (e: React.MouseEvent<HTMLDivElement>) => {
     // Prevent the event from propagating to the document level
     e.stopPropagation();
   };
 
+  const handleThemeChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const selectedTheme = event.target.value;
+
+    switch (selectedTheme) {
+      case "dark":
+        setTheme("dark");
+        break;
+      case "light":
+        setTheme("light");
+        break;
+      case "system":
+        setTheme("system");
+        break;
+      default:
+        console.log("Invalid theme");
+        break;
+    }
+  };
   return (
     <>
       <div className="relative inline-block ">
@@ -63,7 +108,7 @@ function UDropdown({
         {isOpen && (
           <div
             ref={dropdownRef}
-            className="absolute right-0 z-20 py-1 px-1 mt-2 transition-all duration-300 ease-in-out origin-top-right bg-white rounded-xl shadow-xl dark:bg-[#18181B] "
+            className="absolute right-0 z-20 p-2 mt-2 transition-all duration-300 ease-in-out origin-top-right bg-white rounded-xl shadow-xl dark:bg-[#18181B] "
             onClick={handleDropdownClick}
           >
             <div className="h-14 px-4 py-1 gap-2">
@@ -85,6 +130,47 @@ function UDropdown({
             >
               გამოსვლა
             </Button>
+            <Divider className="my-4" />
+            <div className="flex flex-row h-7 items-center space-x-4 text-small">
+              <p className="px-4 py-3 text-sm  justify-between text-gray-600 bg-transparent capitalize transition-colors duration-300 transform dark:text-gray-300   ">
+                Theme
+              </p>
+              <Select
+                className="w-[125px]"
+                size="sm"
+                onChange={handleThemeChange}
+                aria-label="Select theme"
+                labelPlacement="outside"
+                defaultSelectedKeys={[theme || "system"]}
+                startContent={startCon}
+              >
+                <SelectItem
+                  key="dark"
+                  value={"dark"}
+                  startContent={
+                    <MoonFilledIcon size={20} height={20} width={20} />
+                  }
+                >
+                  Dark
+                </SelectItem>
+                <SelectItem
+                  key="light"
+                  value={"light"}
+                  startContent={
+                    <SunFilledIcon size={20} height={20} width={20} />
+                  }
+                >
+                  Light
+                </SelectItem>
+                <SelectItem
+                  key="system"
+                  value={"system"}
+                  startContent={<SystemIcon size={20} height={20} width={20} />}
+                >
+                  System
+                </SelectItem>
+              </Select>
+            </div>
           </div>
         )}
       </div>
