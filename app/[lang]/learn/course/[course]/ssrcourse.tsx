8e7c1209c961ef429f8fd5f45635322b544c0bc0
whosNikoloz <nika.kobaidze021@gmail.com>
2024-02-27 21:57:34 +0400

    Add course and progress fetching logic ssrLearn and Skeletons

--- app/[lang]/learn/course/[course]/ssrcourse.tsx
+++ app/[lang]/learn/course/[course]/ssrcourse.tsx
@@ -15,9 +15,15 @@ import {
   ModalFooter,
   Button,
   useDisclosure,
+  Card,
+  CardBody,
 } from "@nextui-org/react";
 import Link from "next/link";
 import { Locale } from "@/i18n.config";
+import { useQuery } from "react-query";
+import CourseHeroSkeleton from "@/components/Learn/cousreheroskeleton";
+import SubjectSkeleton from "@/components/Learn/subjectSkeleton";
+import { Certification } from "@/components/icons";
 
 interface Course {
   courseId: number;
@@ -45,73 +51,109 @@ export default function SSRCourse({
   const [progress, setProgress] = useState<UserProgress | null>(null);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  useEffect(() => {
-    const fetchCourseAndProgress = async () => {
-      try {
-        const courseResponse = await CoursesAPI().GetCourse(course);
-        console.log(courseResponse);
-        setCourses(courseResponse);
-        if (user) {
-          const userProgress = await ProgressAPI().GetProgress(
-            { userid: user.userId },
-            { courseid: courseResponse.courseId }
-          );
-          setProgress(userProgress);
-        } else {
-          setProgress(null);
-          setTimeout(() => {
-            onOpen(); // Open modal if user is not logged in
-          }, 3000);
-        }
-      } catch (error) {
-        console.error("Error fetching course or user progress:", error);
-      }
-    };
+  const fetchCourseAndProgress = async () => {
+    const courseResponse = await CoursesAPI().GetCourse(course);
+    if (user) {
+      const userProgress = await ProgressAPI().GetProgress(
+        { userid: user.userId },
+        { courseid: courseResponse.courseId }
+      );
+      return { course: courseResponse, progress: userProgress };
+    } else {
+      return { course: courseResponse, progress: null };
+    }
+  };
 
-    const fetchData = async () => {
-      await fetchCourseAndProgress();
-      const intervalId = setInterval(fetchCourseAndProgress, 30000);
-      return () => clearInterval(intervalId);
-    };
-
-    fetchData();
-
-    return () => {
-      setProgress(null);
-    }; // Clean up course and progress on unmount
-  }, [user, onOpen, course]);
-
-  return (
-    <>
-      {courses && (
-        <div className="flex flex-col items-center justify-center  dark:bg-pattern-dark bg-pattern-white text-white">
-          <Hero
-            logo={courses.courseLogo}
-            courseName={courses.courseName}
-            description={courses.description}
-          />
-          <Subject courseData={courses} userProgress={progress} />
-        </div>
-      )}
-      <Modal backdrop="blur" isOpen={isOpen} onClose={onClose}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">!</ModalHeader>
-              <ModalBody>
-                <p>დასაწყებად აუცილებელია ავტორიზაცია</p>
-              </ModalBody>
-              <ModalFooter>
-                <Link href="/user/auth">
-                  <Button color="primary" onPress={onClose}>
-                    შესვლა
-                  </Button>
-                </Link>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </>
+  const { data, isLoading, error } = useQuery(
+    ["courseAndProgress", course, user],
+    fetchCourseAndProgress,
+    {
+      refetchInterval: 30000, // Refetch the data every 30 seconds
+    }
   );
+
+  useEffect(() => {
+    if (!isLoading && !user && (!data || data.progress === null)) {
+      const timeoutId = setTimeout(() => {
+        onOpen();
+      }, 3000);
+      return () => clearTimeout(timeoutId);
+    }
+    if (data) {
+      setCourses(data.course);
+      setProgress(data.progress);
+    }
+  }, [isLoading, user, data, onOpen]);
+
+  if (isLoading || !courses) {
+    return (
+      <>
+        <div className="flex flex-col items-center justify-center  dark:bg-pattern-dark bg-pattern-white text-white">
+          <CourseHeroSkeleton />
+          <SubjectSkeleton />
+          <div className="mt-14 w-full max-w-2xl mx-auto">
+            <Card
+              isBlurred
+              className="border-none  bg-background/60 dark:bg-default-100/50 max-w-[680px] justify-center items-center "
+              shadow="sm"
+            >
+              <CardBody>
+                <div className="flex flex-col items-center">
+                  <Certification size={100} height={0} width={0} />
+                  <h1 className="dark:text-white text-black mt-4">
+                    თქვენი სერთიფიკატი ახლოს არის
+                  </h1>
+                  <p className="dark:text-white text-black mt-3 mb-4">
+                    მშვენივრად აკეთებ! განაგრძეთ სწავლა თქვენი სერტიფიკატის
+                    აღებისთვის!
+                  </p>
+                  <Button
+                    className="bg-blue-600 w-1/2 text-white"
+                    color="primary"
+                    variant="ghost"
+                  >
+                    სერთიფიკატის მიღება
+                  </Button>
+                </div>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        {courses && (
+          <div className="flex flex-col items-center justify-center  dark:bg-pattern-dark bg-pattern-white text-white">
+            <Hero
+              logo={courses.courseLogo}
+              courseName={courses.courseName}
+              description={courses.description}
+            />
+            <Subject courseData={courses} userProgress={progress} />
+          </div>
+        )}
+        <Modal backdrop="blur" isOpen={isOpen} onClose={onClose}>
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">!</ModalHeader>
+                <ModalBody>
+                  <p>დასაწყებად აუცილებელია ავტორიზაცია</p>
+                </ModalBody>
+                <ModalFooter>
+                  <Link href="/user/auth">
+                    <Button color="primary" onPress={onClose}>
+                      შესვლა
+                    </Button>
+                  </Link>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
+      </>
+    );
+  }
 }
---
+++ components/Learn/subjectSkeleton.tsx
@@ -0,0 +1,181 @@
+import { Button, Link } from "@nextui-org/react";
+import { Vector, Run, Locked, Quiz } from "@/components/icons";
+
+function SubjectSkeleton() {
+  return (
+    <>
+      <div className="mt-14 w-full max-w-2xl mx-auto ">
+        <section className="leading-relaxed w-full max-w-screen-xl  mx-auto px-4 md-px-8 ">
+          <h4
+            className={`cursor-pointer pb-5 flex items-center justify-between text-lg dark:text-white text-black  font-medium `}
+          >
+            <div className="flex items-center gap-2">
+              <Run height={25} width={25} size={0} />
+              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48  animate-pulse"></div>
+            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-5 w-5 dark:text-white text-black ml-2 transform transition-transform`}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </h4>
+
+          <div
+            className={`px-5 py-4 bg-white dark:bg-gray-800 shadow justify-between rounded-lg mb-4 `}
+          >
+            <div className="flex flex-row justify-between ">
+              <div className="flex flex-row items-center gap-4">
+                <Quiz size={25} height={0} width={0} />
+                <div className="flex flex-col gap-1 ">
+                  <p className="text-slate-400 text-[8px] lg-text-[10px]">
+                    გაკვეთილი
+                  </p>
+                  <p className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 animate-pulse"></p>
+                </div>
+              </div>
+              <Vector size={30} height={0} width={0} />
+            </div>
+          </div>
+
+          <div
+            className={`px-5 py-4 bg-white dark:bg-gray-800 shadow justify-between rounded-lg mb-4`}
+          >
+            <div className="flex flex-row justify-between ">
+              <div className="flex flex-row items-center gap-4">
+                <Quiz size={25} height={0} width={0} />
+                <div className="flex flex-col gap-1">
+                  <div className="flex flex-col gap-1 ">
+                    <p className="text-slate-400 text-[8px] lg-text-[10px]">
+                      გაკვეთილი
+                    </p>
+                    <p className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 animate-pulse"></p>
+                  </div>
+                  <div className="rounded-2xl font-medium border lg:w-12 lg:h-6 w-10 h-5 flex items-center justify-center">
+                    <p className="text-slate-400 text-[8px] lg-text-[10px]">
+                      XP + 10
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Link className="w-full">
+              <Button
+                className="bg-blue-600 w-full text-white mt-4"
+                color="primary"
+                variant="ghost"
+              >
+                სწავლა
+              </Button>
+            </Link>
+          </div>
+
+          <div
+            className={`px-5 py-4 bg-white dark:bg-gray-800 shadow justify-between rounded-lg mb-4`}
+          >
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-row items-center gap-4">
+                <Quiz size={25} height={0} width={0} />
+                <div className="flex flex-col gap-1">
+                  <div className="flex flex-col gap-1 ">
+                    <p className="text-slate-400 text-[8px] lg-text-[10px]">
+                      გაკვეთილი
+                    </p>
+                    <p className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 animate-pulse"></p>
+                  </div>
+                  <div className="rounded-2xl font-medium border lg:w-12 lg:h-6 w-10 h-5 flex items-center justify-center">
+                    <p className="text-slate-400 text-[8px] lg-text-[10px]">
+                      XP + 10
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <Locked size={25} height={0} width={0} />
+            </div>
+          </div>
+        </section>
+        <section className="leading-relaxed w-full max-w-screen-xl  mx-auto px-4 md-px-8 ">
+          <h4
+            className={`cursor-pointer pb-5 flex items-center justify-between text-lg dark:text-white text-black  font-medium `}
+          >
+            <div className="flex items-center gap-2">
+              <Locked height={25} width={25} size={0} />
+              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48  animate-pulse"></div>
+            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-5 w-5 dark:text-white text-black ml-2 transform transition-transform`}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </h4>
+          <div
+            className={`px-5 py-4 bg-white dark:bg-gray-800 shadow justify-between rounded-lg mb-4`}
+          >
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-row items-center gap-4">
+                <Quiz size={25} height={0} width={0} />
+                <div className="flex flex-col gap-1">
+                  <div className="flex flex-col gap-1 ">
+                    <p className="text-slate-400 text-[8px] lg-text-[10px]">
+                      გაკვეთილი
+                    </p>
+                    <p className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 animate-pulse"></p>
+                  </div>
+                  <div className="rounded-2xl font-medium border lg:w-12 lg:h-6 w-10 h-5 flex items-center justify-center">
+                    <p className="text-slate-400 text-[8px] lg-text-[10px]">
+                      XP + 10
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <Locked size={25} height={0} width={0} />
+            </div>
+          </div>
+
+          <div
+            className={`px-5 py-4 bg-white dark:bg-gray-800 shadow justify-between rounded-lg mb-4`}
+          >
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-row items-center gap-4">
+                <Quiz size={25} height={0} width={0} />
+                <div className="flex flex-col gap-1">
+                  <div className="flex flex-col gap-1 ">
+                    <p className="text-slate-400 text-[8px] lg-text-[10px]">
+                      გაკვეთილი
+                    </p>
+                    <p className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 animate-pulse"></p>
+                  </div>
+                  <div className="rounded-2xl font-medium border lg:w-12 lg:h-6 w-10 h-5 flex items-center justify-center">
+                    <p className="text-slate-400 text-[8px] lg-text-[10px]">
+                      XP + 10
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <Locked size={25} height={0} width={0} />
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default SubjectSkeleton;
